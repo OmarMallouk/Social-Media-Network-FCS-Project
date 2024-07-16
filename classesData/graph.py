82d59e4ccad8a2dd938311495d77bc7867180670
OmarMallouk <mr.mallouk.om@gmail.com>
2024-07-17 02:06:36 +0300

    added a friendship method

--- classesData/graph.py
+++ classesData/graph.py
@@ -12,3 +12,12 @@ class Graph:
             }
         else:
             print(f"User {user_id} already exists in the graph.")
+
+
+    def add_friendship(self, user_id1, user_id2):
+        """Add a friendship (edge) between two users."""
+        if user_id1 in self.graph and user_id2 in self.graph:
+            self.graph[user_id1]["friends"].add(user_id2)
+            self.graph[user_id2]["friends"].add(user_id1)
+        else:
+            print(f"One or both users ({user_id1}, {user_id2}) do not exist in the graph.")
