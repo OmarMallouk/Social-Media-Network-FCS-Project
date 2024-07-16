e3835b011ce14b8d49ddaf1dfe7cb39ce31c2e84
OmarMallouk <mr.mallouk.om@gmail.com>
2024-07-16 22:36:39 +0300

    added another class inside the user to handle the adding and removing of new users

--- classesData/users.py
+++ classesData/users.py
@@ -21,3 +21,16 @@ class User:
         return (f"User(user_id={self.user_id}, name={self.name}, email={self.email}, "
                 f"age={self.age}, friends={list(self.friends)})")
 
+
+class Users:
+    def __init__(self):
+        
+        # Created a dictionary to store the users(the key is user_id, value is the user)
+        self.users = {}
+
+     # method to add a user 
+    def add_user(self, user_id, name, email=None, age=None):
+        if user_id not in self.users:
+            self.users[user_id] = User(user_id, name, email, age)
+        else:
+            print(f"User {user_id} already exists.")
