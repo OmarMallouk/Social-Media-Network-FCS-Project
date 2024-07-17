3668018594416cbb7230bf6fe11c3a95702da2a0
OmarMallouk <mr.mallouk.om@gmail.com>
2024-07-17 22:36:14 +0300

    added a remove method to the graph

--- classesData/graph.py
+++ classesData/graph.py
@@ -1,24 +1,40 @@
 class Graph:
     def __init__(self):
         # Dictionary to store the graph data using adjacency list
-        self.graph = {}  
+        self.users = {}  
 
     def add_user(self, user_id, name):
         """Add a user (vertex) to the graph."""
-        if user_id not in self.graph:
-            self.graph[user_id] = {
+        if user_id not in self.users:
+            self.users[user_id] = {
                 "name": name,
                 "friends": set()
             }
         else:
             print(f"User {user_id} already exists in the graph.")
 
+    def remove_user(self, user_id):
+
+        """Remove a user from the graph."""
+
+        if user_id in self.users:
+
+            del self.users[user_id]
+
+            for user in self.users.values():
+
+                user["friends"].discard(user_id)
+
+        else:
+
+            print(f"User {user_id} does not exist in the graph.")
+
 
     def add_friendship(self, user_id1, user_id2):
         """Add a friendship (edge) between two users."""
-        if user_id1 in self.graph and user_id2 in self.graph:
-            self.graph[user_id1]["friends"].add(user_id2)
-            self.graph[user_id2]["friends"].add(user_id1)
+        if user_id1 in self.users and user_id2 in self.users:
+            self.users[user_id1]["friends"].add(user_id2)
+            self.users[user_id2]["friends"].add(user_id1)
         else:
             print(f"One or both users ({user_id1}, {user_id2}) do not exist in the graph.")
 
