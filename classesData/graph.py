f2d14e5f97e0c8ae8feef394952c0a9dfbf83dc5
OmarMallouk <mr.mallouk.om@gmail.com>
2024-07-18 23:22:38 +0300

    added useful method kekw

--- classesData/graph.py
+++ classesData/graph.py
@@ -1,15 +1,21 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 class Graph:
     def __init__(self):
         # Dictionary to store the graph data using adjacency list
-        self.users = {}  
+        self.adjacency_list = {}  
+        self.graph = nx.Graph()
 
+    #adding user and plotting to the graph
     def add_user(self, user_id, name):
         """Add a user (vertex) to the graph."""
-        if user_id not in self.users:
-            self.users[user_id] = {
+        if user_id not in self.adjacency_list:
+            self.adjacency_list[user_id] = {
                 "name": name,
                 "friends": set()
             }
+            self.graph.add_node(user_id, name=name)
         else:
             print(f"User {user_id} already exists in the graph.")
 
@@ -32,9 +38,9 @@ class Graph:
 
     def add_friendship(self, user_id1, user_id2):
         """Add a friendship (edge) between two users."""
-        if user_id1 in self.users and user_id2 in self.users:
-            self.users[user_id1]["friends"].add(user_id2)
-            self.users[user_id2]["friends"].add(user_id1)
+        if user_id1 in self.adjacency_list and user_id2 in self.adjacency_list:
+            self.adjacency_list[user_id1]["friends"].add(user_id2)
+            self.adjacency_list[user_id2]["friends"].add(user_id1)
         else:
             print(f"One or both users ({user_id1}, {user_id2}) do not exist in the graph.")
 
@@ -43,11 +49,11 @@ class Graph:
 
         """Remove a friendship between two users."""
 
-        if user_id1 in self.users and user_id2 in self.users:
+        if user_id1 in self.adjacency_list and user_id2 in self.adjacency_list:
 
-            self.users[user_id1]["friends"].discard(user_id2)
+            self.adjacency_list[user_id1]["friends"].discard(user_id2)
 
-            self.users[user_id2]["friends"].discard(user_id1)
+            self.adjacency_list[user_id2]["friends"].discard(user_id1)
 
         else:
 
@@ -56,12 +62,12 @@ class Graph:
 
     def get_users(self):
         """Return a list of all users in the graph."""
-        return [(user_id, self.users[user_id]["name"]) for user_id in self.users]
+        return [(user_id, self.adjacency_list[user_id]["name"]) for user_id in self.adjacency_list]
 
     def get_friends(self, user_id):
         """Return a list of friends for a given user."""
-        if user_id in self.users:
-            return [(friend_id, self.users[friend_id]["name"]) for friend_id in self.users[user_id]["friends"]]
+        if user_id in self.adjacency_list:
+            return [(friend_id, self.adjacency_list[friend_id]["name"]) for friend_id in self.adjacency_list[user_id]["friends"]]
         else:
             print(f"User {user_id} does not exist in the graph.")
             return []
